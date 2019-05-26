30a45bdf770172a633ca9fa58d992ea586e9ce41
ayunas <ayunas@gmail.com>
2019-05-26 11:08:42 -0500

    set up successful tests for the four functions in enhancers.js.  used isEqual() to check equality of objects as opposed to toBe() for strings

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,46 @@
 const enhancer = require('./enhancer.js');
 // test away!
+
+const item = {
+    name: 'item1',
+    enhancement: Math.floor(Math.random() * 21),
+    durability: Math.floor(Math.random() * 101)
+}
+
+const item1 = {
+    name: 'item1',
+    enhancement: 15,
+    durability: 51
+}
+
+describe("enhancer.js", () => {
+
+    describe("succeed() function", () => {
+        it("should return a successful enhancement", () => {
+            const succeed = enhancer.succeed(item);
+            expect(succeed).toEqual(item);
+        })
+    })
+
+    describe("fail() function", () => {
+        it("should return a failed enhancement", () => {
+            const fail = enhancer.fail(item);
+            expect(fail).toEqual(item);
+        })
+    })
+
+    describe("repair() function", () => {
+        it("should return a repaired enhancement", () => {
+            const repair = enhancer.repair(item);
+            expect(repair).toEqual(item);
+        })
+    })
+
+    describe("get() function", () => {
+        it("should get the enhancements", () => {
+            const get = enhancer.get(item);
+            expect(get).toEqual(item);
+        })
+    })
+
+})
