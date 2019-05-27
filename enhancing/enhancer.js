04cc0c60109b73f7217a938267af0e84bd0b3215
ayunas <ayunas@gmail.com>
2019-05-27 12:03:03 -0500

    set up tests on all 4 functions.  everything working correctly.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,33 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  return { ...item, enhancement: item.enhancement + 1 };
 }
 
 function fail(item) {
-  return { ...item };
+  console.log('item before failing', item)
+  if (item.enhancement < 15) {
+    item.durability -= 5;
+  } else {
+    item.durability -= 10;
+  }
+  console.log('item after failing', item);
+
+  return item;
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
-  return { ...item };
+  console.log('item in get', item);
+  if (item.enhancement === 0) {
+    return item;
+  } else if (item.enhancement > 0) {
+    item.name = `[+${item.enhancement}] ${item.name}`;
+    return item;
+  }
+
+  return item;
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -7,40 +7,44 @@ const item = {
     durability: Math.floor(Math.random() * 101)
 }
 
-const item1 = {
-    name: 'item1',
-    enhancement: 15,
-    durability: 51
-}
+// const item1 = {
+//     name: 'item1',
+//     enhancement: 15,
+//     durability: 51
+// }
 
 describe("enhancer.js", () => {
 
     describe("succeed() function", () => {
         it("should return a successful enhancement", () => {
             const succeed = enhancer.succeed(item);
-            expect(succeed).toEqual(item);
+            expect(succeed).toEqual({ ...item, enhancement: item.enhancement + 1 });
         })
     })
 
     describe("fail() function", () => {
-        it("should return a failed enhancement", () => {
-            const fail = enhancer.fail(item);
-            expect(fail).toEqual(item);
+        it("should return a failed item, reducing the durability of the item", () => {
+            console.log('item in describe fail()', item);
+            const newItem = { ...item };
+            const fail = enhancer.fail(newItem);
+            console.log('failed item', fail);
+            expect(fail).toEqual({ ...item, durability: (item.durability - 5) });
         })
     })
 
     describe("repair() function", () => {
         it("should return a repaired enhancement", () => {
             const repair = enhancer.repair(item);
-            expect(repair).toEqual(item);
+            expect(repair).toEqual({ ...item, durability: 100 });
         })
     })
 
     describe("get() function", () => {
         it("should get the enhancements", () => {
-            const get = enhancer.get(item);
+            const newItem = { ...item }
+            const get = enhancer.get(newItem);
+            console.log('get: ', get);
             expect(get).toEqual(item);
         })
     })
-
 })
